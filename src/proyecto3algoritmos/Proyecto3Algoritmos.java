74451c417c6caa09518e53ce0d1fa47640419583
Alejandro <Alejandro@Ale>
2017-07-09 21:38:27 -0700

    Proyecto 3 Algoritmos

---
+++ src/proyecto3algoritmos/Proyecto3Algoritmos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto3algoritmos;
+
+import GUI.VentanaPrincipal;
+
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Proyecto3Algoritmos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        VentanaPrincipal v=new VentanaPrincipal();
+        v.setVisible(true);
+
+    }// main
+    
+}// Proyecto3Algoritmos
