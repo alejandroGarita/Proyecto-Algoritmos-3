ba003e671bd7236d31eeb923474dfa097cb13afc
Alejandro <Alejandro@Ale>
2017-07-10 22:21:02 -0700

    Proyecto 3 Algoritmos

--- src/proyecto3algoritmos/Proyecto3Algoritmos.java
+++ src/proyecto3algoritmos/Proyecto3Algoritmos.java
@@ -22,6 +22,36 @@ public class Proyecto3Algoritmos {
         VentanaPrincipal v=new VentanaPrincipal();
         v.setVisible(true);
 
+        
+//        Grafo grafo=new Grafo();
+//        grafo.agregarNodo('a', 0, 0);
+//        grafo.agregarNodo('b', 0, 0);
+//        grafo.agregarNodo('c', 0, 0);
+//        grafo.agregarNodo('d', 0, 0);
+//        grafo.agregarNodo('e', 0, 0);
+//        grafo.agregarNodo('f', 0, 0);
+//        
+//        grafo.agregarAristaNodo('a', 'b', 0);
+//        grafo.agregarAristaNodo('a', 'c', 0);
+//        grafo.agregarAristaNodo('c', 'b', 0);
+//        grafo.agregarAristaNodo('c', 'd', 0);
+//        grafo.agregarAristaNodo('d', 'e', 0);
+//        grafo.agregarAristaNodo('d', 'f', 0);
+//        grafo.agregarAristaNodo('e', 'f', 0);
+//        
+//        grafo.agregarAristaNodo('b', 'a', 0);
+//        grafo.agregarAristaNodo('c', 'a', 0);
+//        grafo.agregarAristaNodo('b', 'c', 0);
+//        grafo.agregarAristaNodo('d', 'c', 0);
+//        grafo.agregarAristaNodo('e', 'd', 0);
+//        grafo.agregarAristaNodo('f', 'd', 0);
+//        grafo.agregarAristaNodo('f', 'e', 0);
+        
+//        grafo.imprimirGrafo();
+        
+//        Primm primm=new Primm(grafo);
+        
+        
     }// main
     
 }// Proyecto3Algoritmos
